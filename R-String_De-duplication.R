44ba7350acd6d92a18872517802d796f93366a4a
Jirka Neumann <jirka.neumann@gmx.de>
2025-11-17 23:49:46 +0100

    janitor library

--- R-String_De-duplication.R
+++ R-String_De-duplication.R
@@ -1,1 +1,17 @@
 #under construction
+library(janitor)
+library(pacman)
+
+
+
+p_load()
+
+xtest = sample(c(1:10), 20, replace = T)
+ytest = sample(c(10:20), 20, replace = T)
+
+test_df = data.frame(xtest, ytest)
+
+print(get_dupes(test_df))
+
+
+
